b8039b86760f0cea7a4d1d33f816407dec5270d0
Adrian Jacobo <adrian.jacobo@wallia.io>
2025-08-05 17:50:52 -0600

    Se crea sliders, pero falta ajustes en imagen y resolver error al borrar

--- api_ecommerce/resources/index.js
+++ api_ecommerce/resources/index.js
@@ -2,8 +2,11 @@ import User from "./User"
 
 import Categorie from "./Categorie"
 import Product from "./Product"
+import Slider from "./Slider"
+
 export default {
     User,
     Categorie,
     Product,
+    Slider
 }
